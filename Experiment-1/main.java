917325041ec96bc7e1e61d90e0491b215507d18a
saikirankanakala <2400030639@kluniversity.in>
2026-01-05 14:50:18 +0530

    added exp-1

---
+++ Experiment-1/main.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+<<<<<<< HEAD
+        System.out.println("Feature update added");
+=======
+        System.out.println("Bug fixed in output");
+>>>>>>> bug-fix
+    }
+}
